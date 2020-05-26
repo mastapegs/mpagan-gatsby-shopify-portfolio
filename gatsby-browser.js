e09c33b000741edc178960ff82add020ad2ec363
Matthew Pagan <mastapegs01@gmail.com>
2020-05-26 15:52:12 -0400

    added apollo and switched to yarn

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,6 @@
 const React = require('react')
+const { ApolloClient, ApolloLink, InMemoryCache, HttpLink } = require('apollo-boost');
+const { ApolloProvider } = require('@apollo/react-hooks')
 const Layout = require('./src/components/Layout').default
 
 exports.wrapPageElement = ({ element, props }) => {
@@ -6,5 +8,29 @@ exports.wrapPageElement = ({ element, props }) => {
     <Layout {...props}>
       {element}
     </Layout>
+  )
+}
+
+const httpLink = new HttpLink({ uri: 'https://graphql.fauna.com/graphql' });
+
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      authorization: `Bearer ${process.env.GATSBY_FAUNA_KEY}`
+    }
+  });
+  return forward(operation);
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+exports.wrapRootElement = ({ element }) => {
+  return (
+    <ApolloProvider client={client}>
+      {element}
+    </ApolloProvider>
   )
 }
