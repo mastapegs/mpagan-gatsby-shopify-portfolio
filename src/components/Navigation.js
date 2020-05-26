69198ff7c5d8c0cdd54c0a0913b7f23d0f73c75e
Matthew Pagan <mastapegs01@gmail.com>
2020-05-26 14:17:26 -0400

    Add an active link state to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { Navbar, Nav } from 'react-bootstrap'
+import { useLocation } from '@reach/router'
 import styles from './Navigation.module.css'
 
 const Navigation = () => {
+  const { pathname } = useLocation()
   const links = [
     {
       href: '/about',
@@ -25,9 +27,9 @@ const Navigation = () => {
         <Navbar.Brand><Link className={styles.link} to='/'>Matthew Pagan</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+          <Nav activeKey={pathname} className="mr-auto">
             {links.map(({ href, linkText }) => (
-              <Nav.Link as="li">
+              <Nav.Link as="li" eventKey={href}>
                 <Link className={styles.link} to={href}>{linkText}</Link>
               </Nav.Link>
             ))}
