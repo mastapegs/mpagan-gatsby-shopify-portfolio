a0f86e7963f10ded15729e7c8fff77d9cb8b441d
Matthew Pagan <mastapegs01@gmail.com>
2020-05-29 22:08:44 -0400

    fix plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,34 +6,20 @@
 
 const path = require(`path`)
 
-plugins: [
-  {
-    resolve: `gatsby-plugin-manifest`,
-    options: {
-      name: `Matthew Pagan`,
-      short_name: `Matthew Pagan`,
-      start_url: `/`,
-      background_color: `#ffffff`,
-      theme_color: `#092cb8`,
-      display: `standalone`
-    }
-  },
-  'gatsby-plugin-offline',
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `images`,
-      path: path.join(__dirname, `src`, `images`),
-    },
-  },
-  `gatsby-plugin-sharp`,
-  `gatsby-transformer-sharp`,
-]
-
-
 module.exports = {
-  /* Your site config here */
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Matthew Pagan`,
+        short_name: `Matthew Pagan`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#092cb8`,
+        display: `standalone`
+      }
+    },
+    'gatsby-plugin-offline',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
